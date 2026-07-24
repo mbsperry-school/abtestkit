0b58edc0497a425706af357331fea536b57714cf
Madi Sperry <madi-sperry@pluralsight.com>
2026-07-23 21:59:21 -0600

    added data cleaning

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# abtestkit
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of abtestkit is to ...
+
+## Installation
+
+You can install the development version of abtestkit from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("pak")
+pak::pak("mbsperry-school/abtestkit")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(abtestkit)
+## basic example code
+```
+
---
+++ data-raw/DATASET.R
@@ -0,0 +1,17 @@
+n<- 5000
+
+raw_data <- read.csv("data-raw/marketing_AB.csv")
+
+set.seed(42)
+
+
+psa_group <- raw_data[raw_data$test.group == "psa", ]
+ad_group <- raw_data[raw_data$test.group == "ad", ]
+
+sample_psa_indices <- sample(nrow(psa_group), size = n, replace = FALSE)
+psa_sample <- psa_group[sample_psa_indices, ]
+
+sample_ad_indices <- sample(nrow(ad_group), size = n, replace = FALSE)
+ad_sample <- ad_group[sample_ad_indices, ]
+
+usethis::use_data(DATASET, overwrite = TRUE)
