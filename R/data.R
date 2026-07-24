318f45ffa280ccd0329a6d2a287f7ef85daee3f9
Madi Sperry <madi-sperry@pluralsight.com>
2026-07-23 22:10:11 -0600

    converted data type

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Marketing AB Test Data
+#'
+#' A sample of 10,000 users from a Kaggle marketing dataset where users were
+#' exposed to either an advertisement or public service announcement. This also
+#' includes whether the user was converted.
+#'
+#' @format A dataframe with 10,000 rows and 2 columns:
+#' \describe{
+#' \item{test_group}{Treatment group: "ad" or "psa}
+#' \item{converted}{Outcome of 1 or 0}}
+#'
+#' @source \url{https://www.kaggle.com/datasets/faviovaz/marketing-ab-testing}
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,9 +1,8 @@
-n<- 5000
+n <- 5000
 
 raw_data <- read.csv("data-raw/marketing_AB.csv")
 
 set.seed(42)
-
 
 psa_group <- raw_data[raw_data$test.group == "psa", ]
 ad_group <- raw_data[raw_data$test.group == "ad", ]
@@ -14,4 +13,9 @@ psa_sample <- psa_group[sample_psa_indices, ]
 sample_ad_indices <- sample(nrow(ad_group), size = n, replace = FALSE)
 ad_sample <- ad_group[sample_ad_indices, ]
 
-usethis::use_data(DATASET, overwrite = TRUE)
+sampled <- rbind(ad_group, psa_group)
+sampled$converted <- as.numeric(raw_data$converted)
+
+sampled_clean <- sampled[, c("test.group", "converted")]
+
+usethis::use_data(sampled_clean, overwrite = TRUE)
